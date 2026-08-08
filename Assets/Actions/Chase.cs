be38ed271af4f1cc2ef36ead03228a5d639ab70e
Christian Mark M. Buena <mrtnztan@Christians-MacBook-Air.local>
2026-08-08 19:03:00 +0800

    add: create  enemy animation

--- Assets/Actions/Chase.cs
+++ Assets/Actions/Chase.cs
@@ -6,32 +6,31 @@ using UnityEngine;
 public class Chase : MonoBehaviour
 {
     [SerializeField] private float chaseSpeed = 5f;
+
     private Rigidbody2D rb;
-    [SerializeField] private Transform target;
+    private Animator animator;
+    private Transform target;
     private Vector2 movementDirection;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         target = GameObject.Find("Player").transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (target)
-        {
-            Vector3 direction = (target.position - transform.position).normalized;
-            movementDirection = direction;
+        if (!target) return;
 
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            rb.rotation = angle;  
-        }
+        movementDirection = (target.position - transform.position).normalized;
+
+        animator.SetFloat("MoveX", movementDirection.x);
+        animator.SetFloat("MoveY", movementDirection.y);
     }
 
     void FixedUpdate()
